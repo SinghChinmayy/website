43736522f9e3dbb60bf8f87acf07f6f12897e20b
Chinmay Singh <singhchinmayy@gmail.com>
2026-01-11 13:54:15 +0530

    modify next.config.ts file for better static file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Optional: Add trailing slashes for better static hosting
+  trailingSlash: true,
 };
 
 export default nextConfig;
